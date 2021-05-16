23ef39151368fa355cf5608da08ee27baeb0ee00
Haqzel <axelr.rizos@gmail.com>
2021-05-16 17:37:10 -0500

    test

--- php/crud/db.php
+++ php/crud/db.php
@@ -1,11 +1,14 @@
 <?php
 include "../config.php";
 echo $host;
-// try {
-// 	$db = new PDO('mysql:host=' . $host . '; dbname=' . $dbname, $username, $password);
-// 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// 	$db->exec("SET CHARACTER SET UTF8");
-// } catch (Exception $e) {
-// 	die("Error" . $e->getMessage());
-// 	echo "error line: " . $e->getLine();
-// }
+echo $dbname;
+echo $username;
+echo $password;
+try {
+	$db = new PDO('mysql:host=' . $host . '; dbname=' . $dbname, $username, $password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("SET CHARACTER SET UTF8");
+} catch (Exception $e) {
+	die("Error" . $e->getMessage());
+	echo "error line: " . $e->getLine();
+}
