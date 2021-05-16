e42784e5b1ababf0d88cbc376182ca91e584110f
Haqzel <axelr.rizos@gmail.com>
2021-05-16 13:08:40 -0500

    ordening php in a folder

---
+++ php/config.php
@@ -0,0 +1,26 @@
+<?php
+///////////////////////////////////////////////
+//	main branch
+///////////////////////////////////////////////
+
+// $host = "localhost";
+// $dbname = "u592367988_angelineBags";
+// $username = "u592367988_angelineBags";
+// $password = "Mo]6ED]yRf";
+// $home = "https://salonrizo.com/tools/angelinebags/crud/";
+// $apiUrl = "https://salonrizo.com/tools/angelinebags/api/";
+// $uploadCarpet = "/tools/angelinebags/uploads/";
+
+///////////////////////////////////////////////
+//	test branch
+///////////////////////////////////////////////
+
+$host = "localhost";
+$dbname = "u592367988_testangelineba";
+$username = "u592367988_testangelineba";
+$password = "MS@/c8=3#3s";
+$home = "https://salonrizo.com/test/angelinebags/crud/";
+$apiUrl = "https://salonrizo.com/test/angelinebags/api/";
+$uploadCarpet = "/test/angelinebags/uploads/";
+
+?>
---
+++ php/crud/db.php
@@ -0,0 +1,11 @@
+<?php
+include "../config.php";
+try {
+	$db = new PDO('mysql:host=' . $host . '; dbname=' . $dbname, $username, $password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("SET CHARACTER SET UTF8");
+} catch (Exception $e) {
+	die("Error" . $e->getMessage());
+	echo "error line: " . $e->getLine();
+}
+
