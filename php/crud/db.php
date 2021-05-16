abba20e3f5b4fe54d8e3e09168de1a2b4ee0152a
Haqzel <axelr.rizos@gmail.com>
2021-05-16 17:32:38 -0500

    db

--- php/crud/db.php
+++ php/crud/db.php
@@ -1,5 +1,6 @@
 <?php
 include "../config.php";
+echo $host;
 try {
 	$db = new PDO('mysql:host=' . $host . '; dbname=' . $dbname, $username, $password);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
