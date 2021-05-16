c35443986bc3d7f56d57d706eeedea542ca32fee
Haqzel <axelr.rizos@gmail.com>
2021-05-16 17:40:03 -0500

    test

--- php/crud/create/registerBag.php
+++ php/crud/create/registerBag.php
@@ -6,18 +6,18 @@ echo $dbname;
 echo $username;
 echo $password;
 
-$description = $_GET["description"];
-$price = $_GET["price"];
-$lenght = $_GET["lenght"];
-$width = $_GET["width"];
-$height = $_GET["height"];
-$handle = $_GET["handle"];
+// $description = $_GET["description"];
+// $price = $_GET["price"];
+// $lenght = $_GET["lenght"];
+// $width = $_GET["width"];
+// $height = $_GET["height"];
+// $handle = $_GET["handle"];
 
-$query = "
-INSERT INTO bags( `description`, `price`, `lenght`, `width`, `height`, `handle`) 
-VALUES ('$description','$price','$lenght','$width','$height','$handle')
-";
+// $query = "
+// INSERT INTO bags( `description`, `price`, `lenght`, `width`, `height`, `handle`) 
+// VALUES ('$description','$price','$lenght','$width','$height','$handle')
+// ";
 
-$db->query($query);
+// $db->query($query);
 
 //header("Location:" . $home);
