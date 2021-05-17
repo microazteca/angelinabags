779dd889d64b7fe6087a466ef40540572a728c5e
AxelRSa <axelg.rizos@gmail.com>
2021-05-16 20:04:29 -0500

    config.php

--- php/crud/config.php
+++ php/crud/config.php
@@ -3,24 +3,24 @@
 //	main branch
 ///////////////////////////////////////////////
 
-// $host = "localhost";
-// $dbname = "u592367988_angelineBags";
-// $username = "u592367988_angelineBags";
-// $password = "Mo]6ED]yRf";
-// $home = "https://salonrizo.com/tools/angelinebags/php/crud/";
-// $apiUrl = "https://salonrizo.com/tools/angelinebags/php/api/";
-// $uploadCarpet = "/tools/angelinebags/uploads/";
+$host = "localhost";
+$dbname = "u592367988_angelineBags";
+$username = "u592367988_angelineBags";
+$password = "Mo]6ED]yRf";
+$home = "https://salonrizo.com/tools/angelinebags/php/crud/";
+$apiUrl = "https://salonrizo.com/tools/angelinebags/php/api/";
+$uploadCarpet = "/tools/angelinebags/uploads/";
 
 ///////////////////////////////////////////////
 //	test branch
 ///////////////////////////////////////////////
 
-$host = "localhost";
-$dbname = "u592367988_testangelineba";
-$username = "u592367988_testangelineba";
-$password = "MS@/c8=3#3s";
-$home = "https://salonrizo.com/test/angelinebags/php/crud/";
-$apiUrl = "https://salonrizo.com/test/angelinebags/php/api/";
-$uploadCarpet = "/test/angelinebags/uploads/";
+// $host = "localhost";
+// $dbname = "u592367988_testangelineba";
+// $username = "u592367988_testangelineba";
+// $password = "MS@/c8=3#3s";
+// $home = "https://salonrizo.com/test/angelinebags/php/crud/";
+// $apiUrl = "https://salonrizo.com/test/angelinebags/php/api/";
+// $uploadCarpet = "/test/angelinebags/uploads/";
 
 ?>
