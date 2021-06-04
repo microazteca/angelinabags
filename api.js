c95d97eb5d68b14a01e0660d2eb677583ae7ba2c
Betania <aguilarperezbetania@gmail.com>
2021-06-03 20:18:42 -0600

    Pasamos de PHP a JavaScript la api del index.
    Co-authored-by: AxelRSa <AxelRSa@users.noreply.github.com>

--- api.js
+++ api.js
@@ -1,18 +1,34 @@
 const aplicacion = document.querySelector(".contenedorProductos");
+const getUrl = new URLSearchParams(window.location.search)
 const url = "https://salonrizo.com/tools/angelinebags/php/api/";
-console.log(url);
 fetch(url)
 	.then(resp => resp.json())
 	.then(data => {
-		data(bolsa => {
-			let img = document.createElement("div")
-			img.innerHTML = bolsa.images
-			const name = document.createElement("div")
-			name.innerHTML = bolsa.description
-			const price = document.createElement("div")
-			price.innerHTML = bolsa.price
-			// p.addEventListener("click", function () {
-			// 	window.location.href = `./bolsa.html?id=${bolsa.id}`
-			// })
+		data.forEach(bolsa => {
+			let container = document.createElement("div")
+			container.innerHTML =
+				`
+				<div class="contenedorProductos">
+					<div class="contenedorProductosImg">
+						<a href="detallesBolsas.html?id=${bolsa.id}">
+							<img src="${bolsa.images[0].url}">
+						</a>
+					</div>
+					<div class="contenedorProductosTexto">
+						<div class="contenedorProductosP">
+							<p>${bolsa.description}</p>
+							<p>$${bolsa.price}.00 MXN</p>
+						</div>
+					</div>
+					<div class = "contenedorBotonDetalles">
+						<a href="detallesBolsas.php">
+       <button>Ver detalles</button>
+      </a>
+					</div>
+				</div>
+				`;
+			container.classList.add("contenedorCatalogoProductos");
+			productosCatalogo = document.querySelector(".productosCatalogo")
+			productosCatalogo.appendChild(container);
 		});
 	})
