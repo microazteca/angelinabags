ceeea06ae66bcff75d2e7255c616911038aa9d8f
Betania <aguilarperezbetania@gmail.com>
2021-06-27 23:29:45 -0600

    Código pasado a BEM pt.2

--- detallesBolsas.js
+++ detallesBolsas.js
@@ -7,34 +7,15 @@ fetch(`${url}/?id=${id}`)
  .then((data) => {
   data[0].images.forEach((images) => {
    let container = document.createElement("div");
-   container.innerHTML = `
-    <div class="contenedorBolsaImagenes">
-     <div class="contenedorIndBolsa">
-      <div class="contenedorImgBolsa">
-       <img class="imgBolsas" src="${images.url}"/>
-							<div class="containerArrow">
-								<div class="containerArrowImgRight">
-									<img class="arrowImgRight"
-									src="../../media/svg/modalArrow.svg"/>
-									</div>
-								<div class="containerArrowImgLeft">	
-									<img class="arrowImgLeft"
-									src="../../media/svg/modalArrow.svg"/>
-									</div>
-								<div class="containerClose">
-									<img class="closeImg"
-									src="../../media/svg/closeImg.svg"/>
-									</div>
-							</div>
-      </div>
-     </div>
-    </div>
+   container.innerHTML = 
+   `
+    <img class="products-container__ind-img" src="${images.url}"/>
 			`;
-   container.classList.add("contenedorBolsaApi");
-   contenedorGrafico = document.querySelector(".contenedorBolsaGrafico");
+   container.classList.add("products-container__ind");
+   contenedorGrafico = document.querySelector(".products-container");
    contenedorGrafico.appendChild(container);
   });
-  document.querySelectorAll(".contenedorImgBolsa img").forEach((el) => {
+  document.querySelectorAll(".products-container img").forEach((el) => {
    el.addEventListener("click", function (ev) {
     ev.stopPropagation();
     this.parentNode.classList.add("modal_active");
@@ -42,8 +23,9 @@ fetch(`${url}/?id=${id}`)
    });
   });
 
-  document.querySelectorAll(".contenedorImgBolsa").forEach((el) => {
+  document.querySelectorAll(".products-container__ind").forEach((el) => {
    el.addEventListener("click", function (ev) {
+    ev.stopPropagation();
     this.classList.remove("modal_active");
    });
   });
@@ -54,49 +36,42 @@ fetch(`${url}/?id=${id}`)
  .then((data) => {
   data.forEach((bolsa) => {
    let container2 = document.createElement("div");
-   container2.innerHTML = `
-				<div class="contenedorBolsaTextos">
-    	<div class="contenedorBolsaNombre">
-     	<h2>${bolsa.description}</h2>
-    	</div>
-    	<div class="contenedorBolsaPrecio">
-     	<h2>$${bolsa.price}.00 MXN</h2>
-    	</div>
-				</div>
+   container2.innerHTML = 
+   `
+    <h2 class="desc-container__desc-name">${bolsa.description}</h2>
+    <h2 class="desc-container__desc-price">$${bolsa.price}.00 MXN</h2>
 			`;
-   container2.classList.add("contenedorBolsaTextos");
-   contenedorBolsaT = document.querySelector(".contenedorBolsaTitulo");
+   container2.classList.add("desc-container__desc");
+   contenedorBolsaT = document.querySelector(".desc-container");
    contenedorBolsaT.appendChild(container2);
 
    let container3 = document.createElement("div");
    container3.innerHTML = `
-			<div class="contenedorCaracteristicasTabla">
-    <table>
-     <tr>
+    <table class="table-container__desc-table">
+     <tr class="table-container__desc-tr">
       <td id="caract">Alto:</td>
       <td id="info">${bolsa.lenght} cms</td>
      </tr>
-     <tr>
+     <tr class="table-container__desc-tr">
       <td id="caract">Largo:</td>
       <td id="info">${bolsa.width} cms</td>
      </tr>
-     <tr>
+     <tr class="table-container__desc-tr">
       <td id="caract">Ancho:</td>
       <td id="info">${bolsa.height} cms</td>
      </tr>
-     <tr>
+     <tr class="table-container__desc-tr">
       <td id="caract">Largo del asa:</td>
       <td id="info">${bolsa.handle} cms</td>
      </tr>
-     <tr>
+     <tr class="table-container__desc-tr">
       <td id="caract">Material:</td>
       <td id="info">Toquilla</td>
      </tr>
     </table>
-   </div>
 			`;
-   container3.classList.add("contenedorCaracteristicasTabla");
-   contenedorTabla = document.querySelector(".contenedorCaracteristicas");
+   container3.classList.add("table-container__desc");
+   contenedorTabla = document.querySelector(".table-container");
    contenedorTabla.appendChild(container3);
   });
  });
